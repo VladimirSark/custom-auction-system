46981ffadbf3d5c158de5aa4802424de61ce7a78
VladimirSark <143695123+VladimirSark@users.noreply.github.com>
2024-10-17 07:44:56 +0300

    Update woocommerce-auction-system.php

--- woocommerce-auction-system.php
+++ woocommerce-auction-system.php
@@ -3,7 +3,7 @@
  * Plugin Name: WooCommerce Auction System
  * Description: A custom WooCommerce plugin for managing auctions.
  * Version: 1.0
- * Author: Vladimir1
+ * Author: Vladimir12
  * Text Domain: woocommerce-auction-system
  */
 
