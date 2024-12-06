49e7dbcbf850b51ff0bee7a6e772b8f5e407a4e4
VladimirSark <143695123+VladimirSark@users.noreply.github.com>
2024-12-06 11:36:47 +0300

    3

--- includes/class-wc-product-bid-cost.php
+++ includes/class-wc-product-bid-cost.php
@@ -4,7 +4,7 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-class WC_Product_Bid_Cost extends WC_Product {
+class WC_Product_Bid_Cost extends WC_Product_Simple {
     public function __construct($product) {
         $this->product_type = 'bid_cost';
         parent::__construct($product);
